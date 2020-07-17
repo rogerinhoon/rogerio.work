d0e88f079bf1c4e39d4135589ddeec6d1b42bfea
Rogerio Oliveira Nascimento <rogerinhoon@gmail.com>
2020-07-17 16:12:59 -0300

    Página em construção

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]> <html class="lt-ie8"> <![endif]-->
+<html xmlns:o="urn:schemas-microsoft-com:office:office" __expr-val-dir="ltr" lang="pt-br" dir="ltr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="x-ua-compatible" content="IE=9">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta http-equiv="Expires" content="0" />
+    <meta name="google-site-verification" content="Y_vZEmxcNKr5YkmxsmZkg7VlfQx8ZEoHbN7ZG8EpfdI" />
+    <title>Rogerio.</title>
+    <meta name="language" content="pt-br">
+    <meta name="description" content="Rogério. Desenvolvimento de websites">
+    <meta name="keywords" content="HTML,CSS,WORDPRESS,JavaScript, Rogério Oliveira, Desenvolvimento de websites">
+    <meta name="author" content="Rogério Oliveira Nascimento">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <style>
+        html,
+        body {
+            padding: 0;
+            margin: 0;
+        }
+
+        body {
+            font-family: 'Raleway', sans-serif;
+        }
+
+        .not-h1 {
+            font-size: 0;
+            margin: 0;
+        }
+
+        .header-rw {
+            text-align: center;
+        }
+
+        .header-rw a {
+            display: inline-block;
+            margin: 20px 10px;
+        }
+
+        p {
+            font-weight: 300;
+        }
+
+        a {
+            color: #000;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #333;
+            text-decoration: none;
+        }
+
+        .content-rw {
+            padding: 10px;
+            text-align: center;
+        }
+
+        .m-0 {
+            margin: 0 !important;
+        }
+
+        .mb-3 {
+            margin-bottom: 7px !important;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+</head>
+
+<body>
+    <h1 class="not-h1">Rogério Oliveira Nascimento</h1>
+    <header class="header-rw">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" title="Rogério Oliveira Nascimento"><img src="assets/img/rogerio.png" alt="Rogério."> </a>
+                </div>
+                <div class="col-md-8">
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="content-rw">
+        <p><strong>Site em construção</strong> </p>
+        <p class="m-0 mb-3">Email: <a href="mailto:contato@rogerio.work">contato@rogerio.work</a> </p>
+        <p class="m-0">Telefone: <a href="https://api.whatsapp.com/send?phone=5521964804294&text=Ol%C3%A1">(21) 96480-4294</a></p>
+    </main>
+</body>
+
+</html>
